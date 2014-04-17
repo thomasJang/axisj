0c79414336ee23d42c5c2366731d7f1bfa97e5e5
tom <tom@axisj.com>
2014-04-17 16:37:39 +0900

    AXUpload5 php code fix

---
+++ samples/AXTree/loadTree.php
@@ -0,0 +1,9 @@
+﻿{
+	result:"ok",
+	tree:[
+		{no:1, nodeName:"LEVEL 1-1", writer:"tom", type:"0", __subTree:true},
+		{no:2, nodeName:"LEVEL 1-2", writer:"tom", type:"5", __subTree:true},
+		{no:3, nodeName:"LEVEL 1-3", writer:"tom", __subTree:true}
+	],
+	msg:""
+}
