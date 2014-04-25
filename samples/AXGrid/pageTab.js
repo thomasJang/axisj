40a4eec51f49917ed755c0c22c59e0092e856bc9
ThomasJ <tom@axisj.com>
2014-04-25 15:26:51 +0900

    AXGrid.setData

--- samples/AXGrid/pageTab.js
+++ samples/AXGrid/pageTab.js
@@ -9,7 +9,10 @@ var myTabOption = [
 	{optionValue:"editor", optionText:"editor", addClass:"", url:"editor.html"},
 	{optionValue:"ajax", optionText:"ajax & paging", addClass:"", url:"ajax.html"},
 	{optionValue:"passive", optionText:"passive", addClass:"", url:"passive.html"},
-	{optionValue:"viewMode", optionText:"viewMode", addClass:"", url:"viewMode.html"}/*,
+	{optionValue:"viewMode", optionText:"viewMode", addClass:"", url:"viewMode.html"},
+    {optionValue:"setData", optionText:"setData way", addClass:"", url:"setData.html"}
+
+	/*,
 	{optionValue:"bigData", optionText:"bigData", addClass:"", url:"bigData.html"}*/
 ];
 
