70cb73d7a59e5a0f5ae10fbd6bc37e13a1731490
ThomasJ <tom@axisj.com>
2014-05-09 11:43:57 +0900

    AXUpload v1.26 fileSelectAutoUpload 옵션 flash 모드에서 작동 하도록 픽스 AXUpload5 sample AXUpload5 manual 예제추가

--- samples/AXUpload5/pageTab.js
+++ samples/AXUpload5/pageTab.js
@@ -3,7 +3,8 @@ var myTabOption = [
 	{optionValue:"AXUpload5", optionText:"AXUpload5", addClass:"", url:"index.html"},
 	{optionValue:"AXUpload5_option", optionText:"AXUpload5(options)", addClass:"", url:"options.html"},
 	{optionValue:"singleUpload", optionText:"singleUpload", addClass:"", url:"single.html"},
-	{optionValue:"selectMainImage", optionText:"selectMainImage", addClass:"", url:"selectMainImage.html"}
+	{optionValue:"selectMainImage", optionText:"selectMainImage", addClass:"", url:"selectMainImage.html"},
+    {optionValue:"manualUpload", optionText:"manual Upload", addClass:"", url:"manualUpload.html"}
 ];
 
 var pageTabChange = function(selectedObject, value){
