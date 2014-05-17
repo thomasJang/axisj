5e512af1396445201482025ef3052c178ed7f377
ThomasJ <tom@axisj.com>
2014-05-17 09:26:30 +0900

    ga update

--- samples/page.js
+++ samples/page.js
@@ -327,10 +327,14 @@ var sampleTreeMenu = new AXTopDownMenu();
 })();
 
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+setTimeout(function(){
+
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-38119279-3', 'axisj.com');
-ga('send', 'pageview');
+	ga('create', 'UA-38119279-3', 'axisj.com');
+	ga('send', 'pageview');
+
+}, 800);
