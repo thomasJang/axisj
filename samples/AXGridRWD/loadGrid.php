58a4d9fa2bd462bc62c823498ed746e88d52e5af
ThomasJ <tom@axisj.com>
2014-05-28 17:25:22 +0900

    ..

--- samples/AXGridRWD/loadGrid.php
+++ samples/AXGridRWD/loadGrid.php
@@ -4,11 +4,9 @@ $pageNo = $_POST["pageNo"];
 {
 result:"ok",
 list:[
-<? if ($pageNo != 2){ ?>
     <? for ($i = 0;$i<20; $i++){ ?>
         <? if($i > 0){ echo ","; } ?>{no:<?=($pageNo-1)*20 + ($i + 1)?>, title:"AXGrid data line <?=$pageNo."/".(($pageNo-1)*20 + ($i + 1))?>", img:"images/<?=$i+1?>.jpg", writer:"Thomas", regDate:"2013-01-18", desc:"myGrid.setList", price:123000, amount:10}
     <? } ?>
-<? } ?>
 ],
 page:{
 pageNo:<?=$pageNo?>,
