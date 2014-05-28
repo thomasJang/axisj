9febf167598c29e0b635a71d91aabcdcd0755895
ThomasJ <tom@axisj.com>
2014-05-28 17:24:12 +0900

    ..

--- samples/AXGridRWD/loadGrid.php
+++ samples/AXGridRWD/loadGrid.php
@@ -1,27 +1,19 @@
 <?php
-
-
 $pageNo = $_POST["pageNo"];
-
-
-
-
 ?>
-
 {
-	result:"ok",
-	list:[
-		<?	
-		for ($i = 1; $i< 20; $i++) {
-			if($i > 1) echo ",";
-			echo "{no:$pageNo$i, title:'AXGrid 세번째 줄 입니다.$i', writer:'장기영$i', regDate:'2013-01-18', img:'img/$i.jpg', desc:'myGrid.setList 의 첫번째 사용법 list json 직접 지정 법', price:123000, amount:2}";
-		}
-		?>
-	],
-	page:{
-		pageNo:<?=$pageNo?>,
-		pageCount:100,	
-		listCount:20
-	},
-	msg:""
+result:"ok",
+list:[
+<? if ($pageNo != 2){ ?>
+    <? for ($i = 0;$i<20; $i++){ ?>
+        <? if($i > 0){ echo ","; } ?>{no:<?=($pageNo-1)*20 + ($i + 1)?>, title:"AXGrid data line <?=$pageNo."/".(($pageNo-1)*20 + ($i + 1))?>", img:"images/<?=$i+1?>.jpg", writer:"Thomas", regDate:"2013-01-18", desc:"myGrid.setList", price:123000, amount:10}
+    <? } ?>
+<? } ?>
+],
+page:{
+pageNo:<?=$pageNo?>,
+pageCount:200,
+listCount:20
+},
+msg:""
 }
